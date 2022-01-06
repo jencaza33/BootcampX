dc13681dc1e4423371c010b4af13623397461af2
Jen Caza <jencaza@gmail.com>
2022-01-06 18:42:08 -0500

    add queries that use node postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`]
+
+pool.query(`
+SELECT DISTINCT cohorts.name as cohort, teachers.name as teacher
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+`, values)
+.then(res => { 
+  console.log('connected')
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
